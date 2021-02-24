01e565977a73b5a87b5e95bc7ed3f790df7badce
AlexFabian10 <78117390+AlexFabian10@users.noreply.github.com>
2021-02-24 17:55:35 -0500

    Add files via upload

---
+++ Herencias/Clases/Cliente.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+   public  class Cliente : Persona 
+    {
+        //ATRIBUTOS 
+        private string CATEGORIA;
+        private string CODIGODEPEDIDO;
+        private string CEDULACLIENTE;
+
+        public string CATEGORIA1
+        {
+            get
+            {
+                return CATEGORIA;
+            }
+
+            set
+            {
+                CATEGORIA = value;
+            }
+        }
+
+        public string CODIGODEPEDIDO1
+        {
+            get
+            {
+                return CODIGODEPEDIDO;
+            }
+
+            set
+            {
+                CODIGODEPEDIDO = value;
+            }
+        }
+
+        public string CEDULACLIENTE1
+        {
+            get
+            {
+                return CEDULACLIENTE;
+            }
+
+            set
+            {
+                CEDULACLIENTE = value;
+            }
+        }
+
+        //PROCEDIMIENTOS, OPERACIONES O METODOS
+        public void GENERARCODIGODEPEDIDO()
+        {
+            this.CODIGODEPEDIDO1 = "C" + this.APPAPATERNO1.Substring(0, 3);
+        }
+    }
+}
---
+++ Herencias/Clases/Empleado.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+   public  class Empleado : Persona 
+    {
+        // ATRIBUTOS
+        private string TIPO;
+        private string TIPOCONTRATO;
+        private double SUELDO;
+        private string CEDULAEMPLEADO;
+        //PROPIEDADES
+        public string TIPO1
+        {
+            get
+            {
+                return TIPO;
+                           }
+
+            set
+            {
+                TIPO = value;
+            }
+        }
+
+        public string TIPOCONTRATO1
+        {
+            get
+            {
+                return TIPOCONTRATO;
+            }
+
+            set
+            {
+                TIPOCONTRATO = value;
+            }
+        }
+
+        public double SUELDO1
+        {
+            get
+            {
+                return SUELDO;
+            }
+
+            set
+            {
+                SUELDO = value;
+            }
+        }
+
+        public string CEDULAEMPLEADO1
+        {
+            get
+            {
+                return CEDULAEMPLEADO;
+            }
+
+            set
+            {
+                CEDULAEMPLEADO = value;
+            }
+        }
+
+        //PROCEDIMIENTO, OPERACIONES O METODOS
+        public void CALCULARSUELDO(double SUELDOBASE)
+        {            
+                if (this.TIPOCONTRATO1 == "Nuevo")
+                {
+                    SUELDO1 = 2 * SUELDOBASE + 500;
+                }
+                else if (this.TIPOCONTRATO1 == "Longevo")
+                {
+                    SUELDO1 = 2 * SUELDOBASE + 1500;
+                }               
+            else
+            {
+                this.SUELDO1 = 0;
+            }
+        }
+        public void GENERARLISTADECEDULA()
+        {
+            this.CEDULAEMPLEADO1 = "ID" + this.CEDULAEMPLEADO1.Substring(0, 10);
+        }
+    }
+}
